6cd9caf36cdcd4c06a2a0d6a5d5335b317d31382
Indigo253931 <indigo253931@gmail.com>
2016-07-12 13:02:15 -0600

    added requests

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(bodyParser.json());
 var candies = []; 
 var nextId= 1;
 
+app.get('/candies', function(req, res){
+	res.send(candies);
+});
+
+
 app.get('/candies/:id', function(req, res){
 	var foundCandy;
 	var targetId = parseInt(req.params.id);
@@ -28,5 +33,18 @@ app.post('/candies', function(req, res){
 	candies.push(newCandy);
 	console.log(newCandy);
 	res.send(newCandy);
+});
+
+app.delete('/candies/:id', function(req, res){
+ console.log('candy delete', req.params);
+  var candyId = req.params.id;
+  // find the index of the destination we want to remove
+  var deleteCandyIndex = candy.findIndex(function(element, index) {
+    return (element._id === parseInt(req.params.id)); //params are strings
+  });
+  console.log('deleting candy with index', deleteCandyIndex);
+  var candyToDelete = candy[deleteCandyIndex];
+  candy.splice(deleteCandyIndex, 1);
+  res.json(candyToDelete);
 });
 app.listen(3000);
